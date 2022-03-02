a48ef42bbf28d1453b2b4ebae6d1cd8e4f9c2eb0
Hepheir <hepheir@gmail.com>
2022-03-02 09:42:38 +0900

    Bind 도움말, 검색 commands to bot

--- larksearch/__init__.py
+++ larksearch/__init__.py
@@ -3,6 +3,10 @@ import logging
 from discord import Intents
 from discord import Game
 from discord.ext.commands import Bot
+from discord.ext.commands import Command
+
+from larksearch.commands.search import command as search_cmd
+from larksearch.commands.help import command as help_cmd
 
 
 COMMAND_PREFIX = "!"
@@ -18,4 +22,6 @@ async def on_ready():
     logging.info(f"Logged in as {bot.user}")
     await bot.change_presence(activity=Game(name=f"{COMMAND_PREFIX}{HELP_COMMAND}"))
 
+bot.add_command(search_cmd)
+bot.add_command(help_cmd)
 bot.run(TOKEN)
